7bb932d9e684f65ae43f03a02ad61f2c1c7c9705
quangnguyenhuynh <quangnguyenhuynh@gmail.com>
2026-07-23 11:45:37 +0700

    Expose EOD-style backfill pipeline

--- src/pipeline/__init__.py
+++ src/pipeline/__init__.py
@@ -1,6 +1,6 @@
 from src.pipeline.init_symbols import init_symbols
 from src.pipeline.fetch_one_day import fetch_one_day, fetch_one_day_with_clients
-from src.pipeline.backfill import backfill
+from src.pipeline.backfill import backfill, run_backfill_pipeline
 from src.pipeline.daily import daily_run
 from src.pipeline.eod import run_eod_pipeline
 from src.pipeline.intraday import run_intraday_pipeline
@@ -8,4 +8,15 @@ from src.pipeline.intraday_ingest import run_intraday_ingest
 
 from src.pipeline.streaming_snapshot import run_streaming_ingest
 
-__all__ = ['init_symbols', 'fetch_one_day', 'fetch_one_day_with_clients', 'backfill', 'daily_run', 'run_eod_pipeline', 'run_intraday_pipeline', 'run_intraday_ingest', 'run_streaming_ingest']
+__all__ = [
+    'init_symbols',
+    'fetch_one_day',
+    'fetch_one_day_with_clients',
+    'backfill',
+    'run_backfill_pipeline',
+    'daily_run',
+    'run_eod_pipeline',
+    'run_intraday_pipeline',
+    'run_intraday_ingest',
+    'run_streaming_ingest',
+]
